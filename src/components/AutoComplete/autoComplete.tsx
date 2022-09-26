02340c8b6a2f4647ff937c43ae2131b77c4b9889
sikleo <937330770@qq.com>
2022-09-26 14:42:22 +0800

    添加inupt新的方法autoComplete

---
+++ src/components/AutoComplete/autoComplete.tsx
@@ -0,0 +1,161 @@
+import React, { FC, useState, useEffect, useRef, ChangeEvent, KeyboardEvent, ReactElement } from 'react'
+import classNames from 'classnames'
+import Input, { InputProps } from '../Input/input'
+import Icon from '../Icon/icon'
+import Transition from '../Transition/transition'
+import useDebounce from '../../hooks/useDebounce'
+import useClickOutside from '../../hooks/useClickOutside'
+
+interface DataSourceObject {
+  value: string;
+}
+export type DataSourceType<T = {}> = T & DataSourceObject
+export interface AutoCompleteProps extends Omit<InputProps, 'onSelect'> {
+  /** fetchSuggestions让用户自己筛选返回结果  考虑不到异步使用promise*/
+  fetchSuggestions: (str: string) => DataSourceType[] | Promise<DataSourceType[]>;
+  /** 选择选中某一项 */
+  onSelect?: (item: DataSourceType) => void;
+  /** 自定义模板 */
+  renderOption?: (item: DataSourceType) => ReactElement;
+}
+
+
+export const AutoComplete: FC<AutoCompleteProps> = (props) => {
+  const {
+    fetchSuggestions,
+    onSelect,
+    value,
+    renderOption,
+    ...restProps
+  } = props
+
+  const [inputValue, setInputValue] = useState(value as string)
+  const [suggestions, setSuggestions] = useState<DataSourceType[]>([])
+  const [loading, setLoading] = useState(false)
+  const [showDropdown, setShowDropdown] = useState(false)
+  const [highlightIndex, setHighlightIndex] = useState(-1)
+  const triggerSearch = useRef(false)
+  const componentRef = useRef<HTMLDivElement>(null)
+  const debouncedValue = useDebounce(inputValue, 300)
+  useClickOutside(componentRef, () => { setSuggestions([])})
+
+  useEffect(() => {
+    if (debouncedValue && triggerSearch.current) {
+      setSuggestions([])
+      const results = fetchSuggestions(debouncedValue)
+      if (results instanceof Promise) {
+        setLoading(true)
+        results.then(data => {
+          setLoading(false)
+          setSuggestions(data)
+          if (data.length > 0) {
+            setShowDropdown(true)
+          }
+        })
+      } else {
+        setSuggestions(results)
+        setShowDropdown(true)
+        if (results.length > 0) {
+          setShowDropdown(true)
+        }
+      }
+    } else {
+      setShowDropdown(false)
+    }
+    setHighlightIndex(-1)
+  }, [debouncedValue, fetchSuggestions])
+
+  const highlight = (index: number) => {
+    if (index < 0) index = 0
+    if (index >= suggestions.length) {
+      index = suggestions.length - 1
+    }
+    setHighlightIndex(index)
+  }
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    switch(e.keyCode) {
+      case 13:
+        if (suggestions[highlightIndex]) {
+          handleSelect(suggestions[highlightIndex])
+        }
+        break
+      case 38:
+        highlight(highlightIndex - 1)
+        break
+      case 40:
+        highlight(highlightIndex + 1)
+        break
+      case 27:
+        setShowDropdown(false)
+        break
+      default:
+        break
+    }
+  }
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.trim()
+    setInputValue(value)
+    triggerSearch.current = true
+  }
+  //onSelect点击之后把选中的填进input框里
+  const handleSelect = (item: DataSourceType) => {
+    setInputValue(item.value)
+    setShowDropdown(false)
+    if (onSelect) {
+      onSelect(item)
+    }
+    triggerSearch.current = false
+  }
+
+  const renderTemplate = (item: DataSourceType) => {
+    return renderOption ? renderOption(item) : item.value
+  }
+
+/**弹出下拉选项菜单 */
+  const generateDropdown = () => {
+    return (
+      <Transition
+        in={showDropdown || loading}
+        animation="zoom-in-top"
+        timeout={300}
+        onExited={() => { setSuggestions([]) }}
+      >
+        <ul className="kiko-suggestion-list">
+          {loading &&
+            <div className="suggestions-loading-icon">
+              <Icon icon="spinner" spin/>
+            </div>
+          }
+          {
+            suggestions.map((item, index) => {
+              const classnames = classNames('suggestion-item', {
+                'is-active': index === highlightIndex
+              })
+              return (
+                <li key={index} className={classnames} onClick={() => handleSelect(item)}>
+                  {renderTemplate(item)}
+                </li>
+              )
+            })
+          }
+      </ul>
+      </Transition>
+    )
+  }
+
+  return (
+    <div className="kiko-auto-complete" ref={componentRef}>
+      <Input
+        value={inputValue}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+        {...restProps}
+      />
+      {/**数组里面大于0就让下拉菜单显示 */}
+      {(suggestions.length > 0) && generateDropdown()}
+    </div>
+  )
+}
+
+export default AutoComplete;
